9b5d57dd6b6b033f0cb99cbd6b50548a655a1947
mridhoalfani <jokshahaha133@gmail.com>
2024-02-26 21:18:51 +0700

    update terbaru

--- resources/views/back/pages/home.blade.php
+++ resources/views/back/pages/home.blade.php
@@ -2,5 +2,155 @@
 @section('pageTitle', isset($pageTitle) ? $pageTitle : 'Home')
 
 @section('content')
-    page content 
+    <div class="page-wrapper">
+        <div class="page-header d-print-none">
+            <div class="container-xl">
+                <div class="row g-2 align-items-center">
+                    <div class="col">
+                        <!-- Page pre-title -->
+                        <div class="page-pretitle">
+                            Overview
+                        </div>
+                        <h2 class="page-title">
+                            Dashboard
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="page-body">
+            <div class="container-xl">
+                <div class="row row-dack row-cards">
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="subheader">Sales</div>
+                                    <div class="ms-auto lh-1">
+                                        <div class="dropdown">
+                                            <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">Last 7 days</a>
+                                            <div class="dropdown-menu dropdown-menu-end">
+                                                <a class="dropdown-item active" href="#">Last 7 days</a>
+                                                <a class="dropdown-item" href="#">Last 30 days</a>
+                                                <a class="dropdown-item" href="#">Last 3 months</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="h1 mb-3">75%</div>
+                                <div class="d-flex mb-2">
+                                    <div>Conversion rate</div>
+                                    <div class="ms-auto">
+                                        <span class="text-green d-inline-flex align-items-center lh-1">
+                                            7% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                <path d="M3 17l6 -6l4 4l8 -8"></path>
+                                                <path d="M14 7l7 0l0 7"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="progress progress-sm">
+                                    <div class="progress-bar bg-primary" style="width: 75%" role="progressbar"
+                                        aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" aria-label="75% Complete">
+                                        <span class="visually-hidden">75% Complete</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div class="subheader">Sales</div>
+                                    <div class="ms-auto lh-1">
+                                        <div class="dropdown">
+                                            <a class="dropdown-toggle text-muted" href="#" data-bs-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">Last 7 days</a>
+                                            <div class="dropdown-menu dropdown-menu-end">
+                                                <a class="dropdown-item active" href="#">Last 7 days</a>
+                                                <a class="dropdown-item" href="#">Last 30 days</a>
+                                                <a class="dropdown-item" href="#">Last 3 months</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="h1 mb-3">75%</div>
+                                <div class="d-flex mb-2">
+                                    <div>Conversion rate</div>
+                                    <div class="ms-auto">
+                                        <span class="text-green d-inline-flex align-items-center lh-1">
+                                            7% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24"
+                                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                                                fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                <path d="M3 17l6 -6l4 4l8 -8"></path>
+                                                <path d="M14 7l7 0l0 7"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="progress progress-sm">
+                                    <div class="progress-bar bg-primary" style="width: 75%" role="progressbar"
+                                        aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" aria-label="75% Complete">
+                                        <span class="visually-hidden">75% Complete</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="mb-3">Using Storage <strong>6854.45 MB </strong>of 8 GB</p>
+                                <div class="progress progress-separated mb-3">
+                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 44%"
+                                        aria-label="Regular"></div>
+                                    <div class="progress-bar bg-info" role="progressbar" style="width: 19%"
+                                        aria-label="System"></div>
+                                    <div class="progress-bar bg-success" role="progressbar" style="width: 9%"
+                                        aria-label="Shared"></div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-auto d-flex align-items-center pe-2">
+                                        <span class="legend me-2 bg-primary"></span>
+                                        <span>Regular</span>
+                                        <span
+                                            class="d-none d-md-inline d-lg-none d-xxl-inline ms-2 text-muted">915MB</span>
+                                    </div>
+                                    <div class="col-auto d-flex align-items-center px-2">
+                                        <span class="legend me-2 bg-info"></span>
+                                        <span>System</span>
+                                        <span
+                                            class="d-none d-md-inline d-lg-none d-xxl-inline ms-2 text-muted">415MB</span>
+                                    </div>
+                                    <div class="col-auto d-flex align-items-center px-2">
+                                        <span class="legend me-2 bg-success"></span>
+                                        <span>Shared</span>
+                                        <span
+                                            class="d-none d-md-inline d-lg-none d-xxl-inline ms-2 text-muted">201MB</span>
+                                    </div>
+                                    <div class="col-auto d-flex align-items-center ps-2">
+                                        <span class="legend me-2"></span>
+                                        <span>Free</span>
+                                        <span
+                                            class="d-none d-md-inline d-lg-none d-xxl-inline ms-2 text-muted">612MB</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
 @endsection
